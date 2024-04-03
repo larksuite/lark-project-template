be25f911b856f2793cd834146ca2fcf32f04bfed
yukaibo.me <yukaibo.me@bytedance.com>
2024-04-03 17:54:28 +0800

    feat: add global type JSSDK

--- project/react-empty/global.d.ts
+++ project/react-empty/global.d.ts
@@ -1,3 +1,11 @@
+import type { SDKClient } from '@lark-project/js-sdk';
+
+declare global {
+  interface Window {
+    JSSDK: SDKClient;
+  }
+}
+
 declare module '*.png';
 declare module '*.jpg';
 declare module '*.jpeg';
