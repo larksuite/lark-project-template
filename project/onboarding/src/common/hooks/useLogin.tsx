877af1295c9df8423537f9eb1233f59b4a07701c
孙嘉顺 <sunjiashun@bytedance.com>
2024-11-22 11:40:54 +0800

    feat: 添加注释

--- project/onboarding/src/common/hooks/useLogin.tsx
+++ project/onboarding/src/common/hooks/useLogin.tsx
@@ -1,4 +1,5 @@
 import { createContext, useContext } from 'react';
 
+// The functions of the plugin can be controlled through isLogin.
 export const LoginContext = createContext(false);
 export const useLogin = () => useContext(LoginContext);
