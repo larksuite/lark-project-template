6497a2622fe58a8efd89c67622fcf20843609527
yukaibo.me <yukaibo.me@bytedance.com>
2024-12-12 20:47:30 +0800

    feat: entry file lazy load

--- project/react-empty/src/features/demo/index.tsx
+++ project/react-empty/src/features/demo/index.tsx
@@ -1,14 +1,23 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
+import ReactDOM from 'react-dom';
 import { createRoot } from 'react-dom/client';
-import App from './App';
 
-export default function main() {
+const App = lazy(() => import('./App'));
+
+export default async function main() {
+  await window.JSSDK.shared.setSharedModules({
+    React,
+    ReactDOM,
+  });
+
   const container = document.createElement('div');
   container.id = 'app';
   document.body.appendChild(container);
   const root = createRoot(container);
 
   root.render(
-    <App />,
+    <Suspense fallback={<div>loading...</div>}>
+      <App />
+    </Suspense>
   );
 }
